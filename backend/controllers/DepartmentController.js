0a15a8ae23284c710884f3851984f39ebcfad88c
Alexandre Tonin <xandetonin@gmail.com>
2024-05-06 22:59:49 -0300

    feat: add methods in the new routes

--- backend/controllers/DepartmentController.js
+++ backend/controllers/DepartmentController.js
@@ -1,28 +1,89 @@
 const Department = require("../models/Department");
 const CreateDepartment = require("../services/CreateDepartment");
+const DeleteDepartment = require("../services/DeleteDepartment");
+const GetDepartment = require("../services/GetDepartment");
+const UpdateDepartment = require("../services/UpdateDepartment");
 
 module.exports = class DepartmentController {
-    static async createDepartment(req, res) {
-        const departmentName = req.body.departmentName;
+  static async createDepartment(req, res) {
+    const departmentName = req.body.departmentName;
 
-        if (!departmentName) {
-            return res.status(400).json({ success: false, error: "Department name is required" });
-        }
-
-        try {
-            const newDepartment = await CreateDepartment(departmentName);
-            return res.status(201).json({ success: true, message: "Department created successfully", data: newDepartment });
-        } catch (error) {
-            return res.status(500).json({ success: false, error: error.message });
-        }
+    if (!departmentName) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Department name is required" });
     }
 
-    static async getDepartment(req, res) {
+    try {
+      const newDepartment = await CreateDepartment(departmentName);
+      return res.status(201).json({
+        success: true,
+        message: "Department created successfully",
+        data: newDepartment,
+      });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error.message });
+    }
+  }
+
+  static async readDepartment(req, res) {
+    const departmentId = req.params.id;
+
+    try {
+      const department = await GetDepartment(departmentId);
+      return res.status(200).json(department);
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error.message });
+    }
+  }
+
+  static async updateDepartment(req, res) {
+    const departmentId = req.params.id;
+    const newData = req.body;
+
+    if (!req.body.name) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Name is required" });
     }
 
-    static async updateDepartment(req, res) {
+    if (req.body.id) {
+      return res
+        .status(400)
+        .json({ success: false, message: "It's not possible update ID" });
     }
 
-    static async deleteDepartment(req, res) {
+    try {
+      UpdateDepartment(departmentId, newData);
+      return res
+        .status(200)
+        .json({
+          success: true,
+          departmentId,
+          message: "Department updated sucessfully",
+        });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error.message });
     }
+  }
+
+  static async deleteDepartment(req, res) {
+    const departmentId = req.params.id;
+
+    try {
+      const department = await DeleteDepartment(departmentId);
+      console.log(department);
+      if (!department) {
+        return res.status(404).json({
+          message: `Department id ${departmentId} not exists on database`,
+        });
+      }
+      return res.status(200).json({
+        success: true,
+        message: `Department ${departmentId} deleted sucessfully`,
+      });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error.message });
+    }
+  }
 };
--- backend/routes/DepartmentRoutes.js
+++ backend/routes/DepartmentRoutes.js
@@ -3,5 +3,8 @@ const DepartmentController = require("../controllers/DepartmentController");
 const router = require("express").Router();
 
 router.post("/create", DepartmentController.createDepartment);
+router.get("/:id", DepartmentController.readDepartment);
+router.patch("/update/:id", DepartmentController.updateDepartment);
+router.delete("/delete/:id", DepartmentController.deleteDepartment);
 
 module.exports = router;
