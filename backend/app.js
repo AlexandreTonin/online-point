568c16216e48da4a108900493c7d618b4c892c74
Alexandre Tonin <xandetonin@gmail.com>
2024-05-06 22:58:24 -0300

    fix: adjust morgan log to short

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 // imports
-const express = require('express')
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 const db = require("./db/dbConnection");
 const morgan = require("morgan");
 
@@ -12,27 +12,26 @@ require("dotenv").config();
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan())
+app.use(morgan("short"));
 
 // models
-const Department = require('./models/Department');
-const Role = require('./models/Role');
-const Employee = require('./models/Employee');
+const Department = require("./models/Department");
+const Role = require("./models/Role");
+const Employee = require("./models/Employee");
 const Record = require("./models/Record");
 
 // routes
-const RecordRoutes = require('./routes/RecordRoutes');
-const AuthRoutes = require('./routes/AuthRoutes');
-const DepartmentRoutes = require('./routes/DepartmentRoutes');
-const RoleRoutes = require('./routes/RoleRoutes');
-app.use('/record', RecordRoutes)
-app.use('/department', DepartmentRoutes)
-app.use('/role', RoleRoutes)
-app.use('/', AuthRoutes)
+const RecordRoutes = require("./routes/RecordRoutes");
+const AuthRoutes = require("./routes/AuthRoutes");
+const DepartmentRoutes = require("./routes/DepartmentRoutes");
+const RoleRoutes = require("./routes/RoleRoutes");
+app.use("/record", RecordRoutes);
+app.use("/department", DepartmentRoutes);
+app.use("/role", RoleRoutes);
+app.use("/", AuthRoutes);
 
 // server
 app.listen(process.env.SERVER_PORT, () => {
-    console.log("Servidor rodando na porta: ", process.env.SERVER_PORT);
-    db.syncDatabase();
+  console.log("Servidor rodando na porta: ", process.env.SERVER_PORT);
+  db.syncDatabase();
 });
-
